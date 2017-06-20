75436347d8a026ebb59e71ae6e7c7cc2c27a8dc8
Liam Globit <guoylyy@gmail.com>
2017-06-21 01:34:03 +0800

    no message

---
+++ week3/day17-homework.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: xxx
+import turtle
+
+def main():
+  #设置一个画面
+  windows = turtle.Screen()
+  #设置背景
+  windows.bgcolor('blue')
+  #生成一个黄色乌龟
+  bran = turtle.Turtle()
+  bran.shape('turtle')
+  bran.color('yellow')
+  #开始你的表演
+
+if __name__ == '__main__':
+  main()
